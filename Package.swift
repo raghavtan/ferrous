44138d87df0b9019e08dbb6585934225043670d1
Raghav Tandon <raghavtan@gmail.com>
2025-03-21 18:01:44 +0100

    update CI
    -
    Signed-off-by: Raghav Tandon <raghavtan@gmail.com>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "ferrous",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v15)
     ],
     products: [
         .executable(name: "ferrous", targets: ["ferrous"])
